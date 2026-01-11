be9ec50dc684a69e18ce0b0cc0dad0e52bcd1eaf
tnsnonis-trade <tns.nonis@gmail.com>
2026-01-11 11:06:14 -0800

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Ensures assets load correctly on GitHub Pages subpaths
+  base: './tandspice', // Ensures assets load correctly on GitHub Pages subpaths
   build: {
     outDir: 'dist',
     rollupOptions: {
